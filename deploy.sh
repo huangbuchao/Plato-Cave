70ceb803bbaa98d253a3d64cd6e699f1d2237ca4
huangbuchao <huangbuchao@gmail.com>
2020-01-05 00:24:44 +0800

    :turtle: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,8 @@ npm run build
 cd docs/.vuepress/dist
 
 git init
-git config user.name 'huangbuchao'
-git config user.email 'huangbuchao@gmail.com'
 git add -A
-git commit -m ':turtle:chore: deploy'
+git commit -m 'deploy'
 
 git push -f git@github.com:huangbuchao/Plato-Cave.git master:gh-pages
 
