bb2f3a19696ffdf2bcc4981f129873c99e68ac2f
huangbuchao <huangbuchao@gmail.com>
2020-01-07 16:07:12 +0800

    :turtle:chore: update actions workflow

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ git config user.name huangbuchao
 git config user.email huangbuchao@gmail.com
 git add -A
 git commit -m 'deploy'
+git remote set-url --push origin https://huangbuchao:${secrets.GITHUB_TOKEN}@github.com/huangbuchao/Plato-Cave.git
 
-git push -f https://${secrets.ACCESS_TOKEN ||`x-access-token:${secrets.GITHUB_TOKEN}`}@github.com/huangbuchao/Plato-Cave.git master:gh-pages
+git push -f https://huangbuchao:${secrets.GITHUB_TOKEN}@github.com/huangbuchao/Plato-Cave.git master:gh-pages
 
 cd -
---
+++ docs/react/runtime.md
@@ -0,0 +1,5 @@
+# react运行时
+
+~~~js
+console.log('runtime');
+~~~
